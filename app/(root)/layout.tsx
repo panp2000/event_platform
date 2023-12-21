ac208cb9ae159f78234fc25571f66c42ae1c505c
Tsuyoshi Suenaga <tsuyoshi.suenaga@gmail.com>
2023-12-21 13:33:09 +0900

    /app/(root)/layout.tsx のミスを修正

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -7,12 +7,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <div className="flex h-screen flex-col">
+    <div className="flex h-screen flex-col">
       <Header />
       <main className="flex-1">{children}</main>
       <Footer />
-      </div>
-    </html>
+    </div>
   );
 }
